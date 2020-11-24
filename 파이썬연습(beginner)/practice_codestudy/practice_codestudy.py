4f8ba0b2a18c481782cab3b0eccf84a4a658d5a3
ljs7473 <ljs7463@naver.com>
2020-11-24 22:08:47 +0900

    python

--- 파이썬연습(beginner)/practice_codestudy/practice_codestudy.py
+++ 파이썬연습(beginner)/practice_codestudy/practice_codestudy.py
@@ -208,3 +208,48 @@
 #     print('스택에서 데이터 추출:', end = ""); print(ret)
 #     print('<스택상태>:', end = ""); print(mystack)
 #     ret = popdata()
+# ***************************************************************************************************************
+
+# 표준 체중을 구하는 프로그램을 작성하시오
+
+# *표준 체중 : 각 개인의 키에 적당한 체중
+
+# (성별에 따른 공식)
+# 남자 : 키(m) x 키(m) x 22
+# 여자 : 키(m) x 키(m) x 21
+
+# 조건 1 : 표준 체중은 별도의 함수 내에서 계산
+#         * 함수명 : std_weight 
+#         * 전달값 : 키(height), 성별(gender)
+# 조건 2 : 표준 체중은 소수점 둘째자리까지 표시
+
+# (출력 예제)
+# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
+
+
+def std_weight(height, gender):
+    if gender == '남자':
+        return height * height * 22
+    else:
+        return height * height * 21
+    
+
+height = 175 #cm
+gender = '남자'
+weight = round(std_weight(height/100, gender), 2)
+
+print('키 {0}cm {1}의 표준 체중은 {2}kg 입니다.'.format(height, gender, weight))
+
+# def std_weight(height, gender):
+#     if gender == '남자':
+#         return height * height * 22
+#     else:
+#         return height * height * 21
+
+
+# height = 175
+# gender = '남자'
+# weight = round(std_weight(height/100, gender), 2)
+# print("키{0}cm {1}의 표준 체중은 {2} 입니다.".format(height, gender, weight))
+
+# *******************************************************************************************************************
